70f0d58dcf34e69e90bf17f144c4314dc1036458
kellyadamtan <kellyadamtan@gmail.com>
2025-06-19 18:06:52 +0700

    fix: restore dilithium_signer.go and update circl dependency for Dilithium Mode3 support

---
+++ fluentum/quantum/dilithium_signer.go
@@ -0,0 +1,43 @@
+package quantum
+
+import (
+	"crypto/rand"
+	"errors"
+
+	"github.com/cloudflare/circl/sign/dilithium"
+)
+
+var (
+	ErrInvalidPrivateKey = errors.New("invalid private key")
+	ErrInvalidPublicKey  = errors.New("invalid public key")
+)
+
+type DilithiumSigner struct{}
+
+func (s *DilithiumSigner) GenerateKeyPair() ([]byte, []byte, error) {
+	pub, priv, err := dilithium.Mode3.GenerateKeyPair(rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+	return pub.Bytes(), priv.Bytes(), nil
+}
+
+func (s *DilithiumSigner) Sign(privKey []byte, msg []byte) ([]byte, error) {
+	priv := dilithium.Mode3.PrivateKeyFromBytes(privKey)
+	if priv == nil {
+		return nil, ErrInvalidPrivateKey
+	}
+	sig, err := priv.Sign(rand.Reader, msg, nil)
+	if err != nil {
+		return nil, err
+	}
+	return sig, nil
+}
+
+func (s *DilithiumSigner) Verify(pubKey []byte, msg []byte, sig []byte) (bool, error) {
+	pub := dilithium.Mode3.PublicKeyFromBytes(pubKey)
+	if pub == nil {
+		return false, ErrInvalidPublicKey
+	}
+	return pub.Verify(msg, sig), nil
+}
