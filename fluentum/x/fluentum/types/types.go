d6314a63c295520d207721d8bbc98b200e33728d
kellyadamtan <kellyadamtan@gmail.com>
2025-06-22 11:02:54 +0700

    Fix protobuf methods and query types: add ProtoMessage, Reset, String methods and proper fields

--- fluentum/x/fluentum/types/types.go
+++ fluentum/x/fluentum/types/types.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -265,17 +266,77 @@ func ParamKeyTable() paramtypes.KeyTable {
 
 // Stub query types to fix build errors
 type QueryParamsRequest struct{}
+
+func (m *QueryParamsRequest) Reset()         { *m = QueryParamsRequest{} }
+func (m *QueryParamsRequest) String() string { return "QueryParamsRequest" }
+func (m *QueryParamsRequest) ProtoMessage()  {}
+
 type QueryParamsResponse struct{}
-type QueryAllFluentumRequest struct{}
-type QueryAllFluentumResponse struct{}
-type QueryGetFluentumRequest struct{}
-type QueryGetFluentumResponse struct{}
+
+func (m *QueryParamsResponse) Reset()         { *m = QueryParamsResponse{} }
+func (m *QueryParamsResponse) String() string { return "QueryParamsResponse" }
+func (m *QueryParamsResponse) ProtoMessage()  {}
+
+type QueryAllFluentumRequest struct {
+	Pagination *PageRequest `protobuf:"bytes,1,opt,name=pagination,proto3" json:"pagination,omitempty"`
+}
+
+func (m *QueryAllFluentumRequest) Reset()         { *m = QueryAllFluentumRequest{} }
+func (m *QueryAllFluentumRequest) String() string { return "QueryAllFluentumRequest" }
+func (m *QueryAllFluentumRequest) ProtoMessage()  {}
+
+type QueryAllFluentumResponse struct {
+	Fluentum   []*Fluentum   `protobuf:"bytes,1,rep,name=fluentum,proto3" json:"fluentum,omitempty"`
+	Pagination *PageResponse `protobuf:"bytes,2,opt,name=pagination,proto3" json:"pagination,omitempty"`
+}
+
+func (m *QueryAllFluentumResponse) Reset()         { *m = QueryAllFluentumResponse{} }
+func (m *QueryAllFluentumResponse) String() string { return "QueryAllFluentumResponse" }
+func (m *QueryAllFluentumResponse) ProtoMessage()  {}
+
+type QueryGetFluentumRequest struct {
+	Index string `protobuf:"bytes,1,opt,name=index,proto3" json:"index,omitempty"`
+}
+
+func (m *QueryGetFluentumRequest) Reset()         { *m = QueryGetFluentumRequest{} }
+func (m *QueryGetFluentumRequest) String() string { return "QueryGetFluentumRequest" }
+func (m *QueryGetFluentumRequest) ProtoMessage()  {}
+
+type QueryGetFluentumResponse struct {
+	Fluentum *Fluentum `protobuf:"bytes,1,opt,name=fluentum,proto3" json:"fluentum,omitempty"`
+}
+
+func (m *QueryGetFluentumResponse) Reset()         { *m = QueryGetFluentumResponse{} }
+func (m *QueryGetFluentumResponse) String() string { return "QueryGetFluentumResponse" }
+func (m *QueryGetFluentumResponse) ProtoMessage()  {}
+
+// PageRequest and PageResponse for pagination
+type PageRequest struct {
+	Key        []byte `protobuf:"bytes,1,opt,name=key,proto3" json:"key,omitempty"`
+	Offset     uint64 `protobuf:"varint,2,opt,name=offset,proto3" json:"offset,omitempty"`
+	Limit      uint64 `protobuf:"varint,3,opt,name=limit,proto3" json:"limit,omitempty"`
+	CountTotal bool   `protobuf:"varint,4,opt,name=count_total,json=countTotal,proto3" json:"count_total,omitempty"`
+	Reverse    bool   `protobuf:"varint,5,opt,name=reverse,proto3" json:"reverse,omitempty"`
+}
+
+func (m *PageRequest) Reset()         { *m = PageRequest{} }
+func (m *PageRequest) String() string { return "PageRequest" }
+func (m *PageRequest) ProtoMessage()  {}
+
+type PageResponse struct {
+	NextKey []byte `protobuf:"bytes,1,opt,name=next_key,json=nextKey,proto3" json:"next_key,omitempty"`
+	Total   uint64 `protobuf:"varint,2,opt,name=total,proto3" json:"total,omitempty"`
+}
+
+func (m *PageResponse) Reset()         { *m = PageResponse{} }
+func (m *PageResponse) String() string { return "PageResponse" }
+func (m *PageResponse) ProtoMessage()  {}
 
 // Stub query client interface
 type QueryClient interface {
-	Params(ctx interface{}, in *QueryParamsRequest, opts ...interface{}) (*QueryParamsResponse, error)
-	FluentumAll(ctx interface{}, in *QueryAllFluentumRequest, opts ...interface{}) (*QueryAllFluentumResponse, error)
-	Fluentum(ctx interface{}, in *QueryGetFluentumRequest, opts ...interface{}) (*QueryGetFluentumResponse, error)
+	Params(ctx context.Context, in *QueryParamsRequest, opts ...interface{}) (*QueryParamsResponse, error)
+	FluentumAll(ctx context.Context, in *QueryAllFluentumRequest, opts ...interface{}) (*QueryAllFluentumResponse, error)
+	Fluentum(ctx context.Context, in *QueryGetFluentumRequest, opts ...interface{}) (*QueryGetFluentumResponse, error)
 }
 
 // NewQueryClient creates a new query client
@@ -287,14 +348,19 @@ type queryClient struct {
 	cc interface{}
 }
 
-func (c *queryClient) Params(ctx interface{}, in *QueryParamsRequest, opts ...interface{}) (*QueryParamsResponse, error) {
+func (c *queryClient) Params(ctx context.Context, in *QueryParamsRequest, opts ...interface{}) (*QueryParamsResponse, error) {
 	return &QueryParamsResponse{}, nil
 }
 
-func (c *queryClient) FluentumAll(ctx interface{}, in *QueryAllFluentumRequest, opts ...interface{}) (*QueryAllFluentumResponse, error) {
-	return &QueryAllFluentumResponse{}, nil
+func (c *queryClient) FluentumAll(ctx context.Context, in *QueryAllFluentumRequest, opts ...interface{}) (*QueryAllFluentumResponse, error) {
+	return &QueryAllFluentumResponse{
+		Fluentum:   []*Fluentum{},
+		Pagination: &PageResponse{},
+	}, nil
 }
 
-func (c *queryClient) Fluentum(ctx interface{}, in *QueryGetFluentumRequest, opts ...interface{}) (*QueryGetFluentumResponse, error) {
-	return &QueryGetFluentumResponse{}, nil
+func (c *queryClient) Fluentum(ctx context.Context, in *QueryGetFluentumRequest, opts ...interface{}) (*QueryGetFluentumResponse, error) {
+	return &QueryGetFluentumResponse{
+		Fluentum: &Fluentum{},
+	}, nil
 }
