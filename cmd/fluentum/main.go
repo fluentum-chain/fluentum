12fb467d749d705e68920964fc99b567e43afece
kellyadamtan <kellyadamtan@gmail.com>
2025-06-22 17:53:47 +0700

    Fix compilation errors in main.go: import conflicts, missing types, and interface implementation

--- cmd/fluentum/main.go
+++ cmd/fluentum/main.go
@@ -6,11 +6,10 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/config"
+	clientconfig "github.com/cosmos/cosmos-sdk/client/config"
 	"github.com/cosmos/cosmos-sdk/client/debug"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/keys"
@@ -19,7 +18,7 @@ import (
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
-	"github.com/cosmos/cosmos-sdk/x/auth/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/cosmos/cosmos-sdk/x/crisis"
 	"github.com/cosmos/cosmos-sdk/x/genutil"
@@ -71,7 +70,7 @@ for high throughput and security. It features:
 				return err
 			}
 
-			initClientCtx, err = config.ReadFromClientConfig(initClientCtx)
+			initClientCtx, err = clientconfig.ReadFromClientConfig(initClientCtx)
 			if err != nil {
 				return err
 			}
@@ -179,7 +178,7 @@ type appCreator struct {
 	encCfg app.EncodingConfig
 }
 
-func (a appCreator) newApp(logger log.Logger, db dbm.DB, traceStore io.Writer, appOpts servertypes.AppOptions) servertypes.Application {
+func (a appCreator) NewApp(logger log.Logger, db dbm.DB, traceStore io.Writer, appOpts servertypes.AppOptions) servertypes.Application {
 	// var cache sdk.MultiStorePersistentCache // Removed - not available in v0.50.6
 
 	// if cast.ToBool(appOpts.Get(server.FlagInterBlockCache)) {
@@ -196,20 +195,8 @@ func (a appCreator) newApp(logger log.Logger, db dbm.DB, traceStore io.Writer, a
 		panic(err)
 	}
 
-	snapshotDir := filepath.Join(cast.ToString(appOpts.Get(flags.FlagHome)), "data", "snapshots")
+	// snapshotDir := filepath.Join(cast.ToString(appOpts.Get(flags.FlagHome)), "data", "snapshots")
 	// snapshotDB, err := sdk.NewLevelDB("metadata", snapshotDir) // Removed - not available in v0.50.6
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// snapshotStore, err := snapshots.NewStore(snapshotDB, snapshotDir)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// snapshotOptions := snapshottypes.NewSnapshotOptions(
-	// 	cast.ToUint64(appOpts.Get(server.FlagStateSyncSnapshotInterval)),
-	// 	cast.ToUint32(appOpts.Get(server.FlagStateSyncSnapshotKeepRecent)),
-	// )
 
 	return app.New(
 		logger, db, traceStore, true, skipUpgradeHeights,
@@ -222,13 +209,10 @@ func (a appCreator) newApp(logger log.Logger, db dbm.DB, traceStore io.Writer, a
 		baseapp.SetHaltHeight(cast.ToUint64(appOpts.Get(server.FlagHaltHeight))),
 		baseapp.SetHaltTime(cast.ToUint64(appOpts.Get(server.FlagHaltTime))),
 		baseapp.SetMinRetainBlocks(cast.ToUint64(appOpts.Get(server.FlagMinRetainBlocks))),
-		// baseapp.SetInterBlockCache(cache), // Removed
+		// baseapp.SetInterBlockCache(cache), // cache not defined
 		baseapp.SetTrace(cast.ToBool(appOpts.Get(server.FlagTrace))),
 		baseapp.SetIndexEvents(cast.ToStringSlice(appOpts.Get(server.FlagIndexEvents))),
-		// baseapp.SetSnapshotStore(snapshotStore), // Removed
-		// baseapp.SetSnapshotOptions(snapshotOptions), // Removed
-		baseapp.SetIAVLCacheSize(cast.ToInt(appOpts.Get(server.FlagIAVLCacheSize))),
-		baseapp.SetIAVLDisableFastNode(cast.ToBool(appOpts.Get(server.FlagDisableIAVLFastNode))),
+		// baseapp.SetSnapshot(snapshotStore, snapshotOptions), // snapshotStore and snapshotOptions not defined
 	)
 }
 
@@ -243,10 +227,6 @@ type AppExporter interface {
 }
 
 // Implement the interfaces
-func (a appCreator) NewApp(logger log.Logger, db dbm.DB, traceStore io.Writer, appOpts servertypes.AppOptions) servertypes.Application {
-	return a.newApp(logger, db, traceStore, appOpts)
-}
-
 func (a appCreator) ExportApp(logger log.Logger, db dbm.DB, traceStore io.Writer, height int64, forZeroHeight bool, jailAllowedAddrs []string, appOpts servertypes.AppOptions) (servertypes.ExportedApp, error) {
 	return a.appExport(logger, db, traceStore, height, forZeroHeight, jailAllowedAddrs, appOpts)
 }
