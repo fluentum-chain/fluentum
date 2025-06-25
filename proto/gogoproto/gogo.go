e933055ed6ca653f6609175193f6528e0c1f4c6c
kellyadamtan <kellyadamtan@gmail.com>
2025-06-25 09:32:21 +0700

    Add gogoproto stub file to fix import issues

---
+++ proto/gogoproto/gogo.go
@@ -0,0 +1,5 @@
+// Package gogoproto provides gogoproto compatibility
+package gogoproto
+
+// This is a stub file to satisfy imports from generated protobuf files
+// The actual gogoproto functionality is provided by github.com/gogo/protobuf/gogoproto
