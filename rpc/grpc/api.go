0a2432a0ef38e672f5e2ff630418f341543fa39a
kellyadamtan <kellyadamtan@gmail.com>
2025-06-28 09:58:04 +0700

    Fix RPC gRPC package name to use grpc consistently

--- rpc/grpc/api.go
+++ rpc/grpc/api.go
@@ -4,21 +4,21 @@ import (
 	"context"
 
 	abci "github.com/fluentum-chain/fluentum/proto/tendermint/abci"
-	coregrpc "github.com/fluentum-chain/fluentum/proto/tendermint/rpc/grpc"
+	grpc "github.com/fluentum-chain/fluentum/proto/tendermint/rpc/grpc"
 	core "github.com/fluentum-chain/fluentum/rpc/core"
 	rpctypes "github.com/fluentum-chain/fluentum/rpc/jsonrpc/types"
 )
 
 type broadcastAPI struct {
-	coregrpc.UnimplementedBroadcastAPIServer
+	grpc.UnimplementedBroadcastAPIServer
 }
 
-func (bapi *broadcastAPI) Ping(ctx context.Context, req *coregrpc.RequestPing) (*coregrpc.ResponsePing, error) {
+func (bapi *broadcastAPI) Ping(ctx context.Context, req *grpc.RequestPing) (*grpc.ResponsePing, error) {
 	// kvstore so we can check if the server is up
-	return &coregrpc.ResponsePing{}, nil
+	return &grpc.ResponsePing{}, nil
 }
 
-func (bapi *broadcastAPI) BroadcastTx(ctx context.Context, req *coregrpc.RequestBroadcastTx) (*coregrpc.ResponseBroadcastTx, error) {
+func (bapi *broadcastAPI) BroadcastTx(ctx context.Context, req *grpc.RequestBroadcastTx) (*grpc.ResponseBroadcastTx, error) {
 	// NOTE: there's no way to get client's remote address
 	// see https://stackoverflow.com/questions/33684570/session-and-remote-ip-address-in-grpc-go
 	res, err := core.BroadcastTxCommit(&rpctypes.Context{}, req.Tx)
@@ -26,7 +26,7 @@ func (bapi *broadcastAPI) BroadcastTx(ctx context.Context, req *coregrpc.Request
 		return nil, err
 	}
 
-	return &coregrpc.ResponseBroadcastTx{
+	return &grpc.ResponseBroadcastTx{
 		CheckTx: &abci.ResponseCheckTx{
 			Code: res.CheckTx.Code,
 			Data: res.CheckTx.Data,
