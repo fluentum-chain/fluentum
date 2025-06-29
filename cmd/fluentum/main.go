4aeae9a1f39815e9f95f33fa29aa10e89373537b
kellyadamtan <kellyadamtan@gmail.com>
2025-06-29 10:28:14 +0700

    Fix genesis app_hash to match application state during initialization

--- cmd/fluentum/main.go
+++ cmd/fluentum/main.go
@@ -877,7 +877,7 @@ func initializeNode(homeDir, moniker, chainID string) error {
 					"power": "10",
 				},
 			},
-			"app_hash":  "",
+			"app_hash":  "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855",
 			"app_state": map[string]interface{}{},
 		}
 
